d3eb3a3b0ac4adfd50f2f81d632e57d852028b82
Mustafa Mahmoud <mmh.ustafa9@gmail.com>
2026-04-10 15:48:58 +0200

    add expsystem

---
+++ backend-api/AI-Derma/AI-Derma.Core/Models/Feedback.cs
@@ -0,0 +1,15 @@
+﻿namespace AI_Derma.Core.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public int ResultId { get; set; }
+        public DiagnosticResult Result { get; set; }
+        public int Rating { get; set; } 
+        public string Comments { get; set; }
+     
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}

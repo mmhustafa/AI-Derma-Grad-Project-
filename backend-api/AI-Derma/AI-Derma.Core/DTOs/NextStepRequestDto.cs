d1d285f6878e26d0fa7190b90a65b50a6dbd145f
Mustafa Mahmoud <mmh.ustafa9@gmail.com>
2026-04-13 13:23:59 +0200

    Add Diagonistic Dtos

---
+++ backend-api/AI-Derma/AI-Derma.Core/DTOs/NextStepRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AI_Derma.Core.DTOs
+{
+    public class NextStepRequestDto
+    {
+        public List<string> Facts { get; set; } = new();
+        public int? DiagnosticResultId { get; set; }
+    }
+}

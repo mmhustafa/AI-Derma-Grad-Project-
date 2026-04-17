43e10a69fddeffe3ed767431d5afed65d51659ce
Mustafa Mahmoud <mmh.ustafa9@gmail.com>
2026-04-17 13:33:56 +0200

    update-dbmodels

--- backend-api/AI-Derma/AI-Derma.Infrastructure/Migrations/20260417113202_updatediseasestable.Designer.cs
+++ backend-api/AI-Derma/AI-Derma.Infrastructure/Migrations/20260417113202_updatediseasestable.Designer.cs
@@ -4,6 +4,7 @@ using AI_Derma.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AI_Derma.Infrastructure.Migrations
 {
     [DbContext(typeof(DermaDbContext))]
-    partial class DermaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260417113202_updatediseasestable")]
+    partial class updatediseasestable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
